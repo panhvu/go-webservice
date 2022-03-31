741da73e5407bdf0500c7df50642e46e107fead3
anhvu <phuong-anh.vu@thermondo.de>
2022-03-31 16:32:10 +0200

    added post request handling

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -40,14 +41,55 @@ func init() {
 func productsHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		productsJson, err := json.Marshal(productList)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(productsJson)
+		handleGetRequest(w)
+
+	case http.MethodPost:
+		handlePostRequest(w, r)
 	}
+}
+
+func handleGetRequest(w http.ResponseWriter) {
+	productsJson, err := json.Marshal(productList)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(productsJson)
+}
+
+func handlePostRequest(w http.ResponseWriter, r *http.Request) {
+	// add a new product to the list
+	var newProduct Product
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	err = json.Unmarshal(bodyBytes, &newProduct)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// make sure client didn't send productID, which shall be generated from server
+	if newProduct.ProductID != 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	newProduct.ProductID = getNextID()
+	productList = append(productList, newProduct)
+	w.WriteHeader(http.StatusCreated)
+	return
+}
+
+func getNextID() int {
+	highestID := -1
+	for _, product := range productList {
+		if highestID < product.ProductID {
+			highestID = product.ProductID
+		}
+	}
+	return highestID + 1
 }
 
 func main() {
