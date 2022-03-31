562a80a5b5f23efa39ce3dddea151820dee791dd
anhvu <phuong-anh.vu@thermondo.de>
2022-03-31 16:16:31 +0200

    added /products GET endpoint with dummy JSON data

--- main.go
+++ main.go
@@ -1,23 +1,56 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 )
 
-type fooHandler struct {
-	Message string
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productName"`
 }
 
-func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(f.Message))
+var productList []Product
+
+func init() {
+	// local data from memory
+	productsJson := `[
+		{
+			"productId": 1,
+			"manufacturer": "Johns-Jenkins",
+			"sku": "2924d9",
+			"upc": "39449393",
+			"pricePerUnit": "43.98",
+			"quantityOnHand": 2040,
+			"productName": "Product"
+		}
+	]`
+	err := json.Unmarshal([]byte(productsJson), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func barHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("bar called"))
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productsJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productsJson)
+	}
 }
 
 func main() {
-	http.Handle("/foo", &fooHandler{Message: "foo called"})
-	http.HandleFunc("/bar", barHandler)
+	http.HandleFunc("/products", productsHandler)
 	http.ListenAndServe(":5000", nil)
 }
